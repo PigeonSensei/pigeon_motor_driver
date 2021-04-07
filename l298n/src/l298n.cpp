7539bc77d429ec49342d416e030f068068aeb771
PigeonSensei <parkour5813@gmail.com>
2021-04-07 23:27:36 +0900

    -

--- l298n/src/l298n.cpp
+++ l298n/src/l298n.cpp
@@ -163,30 +163,6 @@ int L298N::Spin()
   return 0;
 }
 
-//int ReturnInputKey()
-//{
-//  struct termios org_term;
-
-//  char input_key = 0;
-
-//  tcgetattr(STDIN_FILENO, &org_term);
-
-//  struct termios new_term = org_term;
-
-//  new_term.c_lflag &= ~(ECHO | ICANON);
-
-//  new_term.c_cc[VMIN] = 0;
-//  new_term.c_cc[VTIME] = 0;
-
-//  tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
-
-//  read(STDIN_FILENO, &input_key, 1);
-
-//  tcsetattr(STDIN_FILENO, TCSANOW, &org_term);
-
-//  return input_key;
-//}
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "l298n_node");
@@ -204,7 +180,6 @@ int main(int argc, char **argv)
 
     ros::spinOnce();
     loop_rate.sleep();
-//    if(ReturnInputKey() == 27) break;
   }
 
   return 0;
